921eb1d5c2b7ea015fa05eb3b9c2bee2666405cc
xoops-magazine <xoops-magazine@tld.com>
2008-05-20 12:06:10 +0000

    ask.com added
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/social_bookmarks/trunk@2287 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- xoops_version.php
+++ xoops_version.php
@@ -279,6 +279,14 @@ $modversion['config'][$i]['valuetype']	= 'int';
 $modversion['config'][$i]['default']	= 1;
 $modversion['config'][$i]['description']= '';
 
+$i++;
+$modversion['config'][$i]['name']		= 'book_ask';  // www.ask.com/
+$modversion['config'][$i]['title']		= "_MD_BOOKMARKS_LI_ASK";
+$modversion['config'][$i]['formtype']	= 'yesno';
+$modversion['config'][$i]['valuetype']	= 'int';
+$modversion['config'][$i]['default']	= 1;
+$modversion['config'][$i]['description']= '';
+
 unset($i);
 
 ?>
